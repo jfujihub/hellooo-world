5b23d032e61543b7fb68d2ee9767e138df64fa4c
jfuji <jfuji@xs36vl>
2014-07-21 13:53:07 +0900

    add multiplication table

--- hello.c
+++ hello.c
@@ -4,7 +4,18 @@
 
 int main(void)
 {
+	int i, j;
+
+	/* あいさつを印字する */
 	printf("Hello, world.\n");
+
+	/* 九九を印字する */
+	for (i = 1; i <= 9; i++) {
+		for (j = 1; j <= 9; j++) {
+			printf("%2d ", i * j);
+		}
+		putchar('\n');
+	}
 
 	return EXIT_SUCCESS;
 }
